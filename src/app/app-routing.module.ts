331cdea628dba48764a9e558ec81761e9dd6d013
kaushalkumar8459 <kaushalkumar8459@gmail.com>
2021-01-19 09:45:37 +0530

    changes in styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,17 +5,13 @@ const routes: Routes = [
   { path: '', redirectTo: '/GitRepo', pathMatch: 'full' },
   {
     path: 'GitRepo',
-    //angular
     loadChildren: () =>
       import(`./module/gitrepo/gitrepo.module`).then((m) => m.GitrepoModule),
-
-    // lazy loading in angular 7
-    // loadChildren: './modules/signup/signup.module#SignupModule',
   },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes),RouterModule],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes), RouterModule],
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
